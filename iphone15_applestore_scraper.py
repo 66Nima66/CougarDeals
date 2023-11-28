b6cc656d84fa8ebf277e3f5c80e9c67dc413d4bd
Michael Cassidy <111249267+King-Cass@users.noreply.github.com>
2023-11-28 15:15:57 -0500

    Update iphone15_applestore_scraper.py
    added comments for readability

--- iphone15_applestore_scraper.py
+++ iphone15_applestore_scraper.py
@@ -8,7 +8,7 @@ from firebase_admin import credentials, firestore
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Initialize Firebase Admin
+# Initialize Firebase Admin  CHANGE TO YOUR SPECIFIC DB!!!!!!!!!!!!!!!!!
 cred = credentials.Certificate('/Users/michaelcassidy/Downloads/cougar-phone-deals-firebase-adminsdk-56e88-2edf050505.json')
 firebase_admin.initialize_app(cred)
 
@@ -25,18 +25,18 @@ try:
     # Open the webpage
     driver.get(url)
 
-    # Wait for the JavaScript to populate the price data
+    # Look for the css selector
     price_css_selector = "span.sosumi-link-follows[data-pricing-product='iphone-15']"
 
-    # Wait for the price element to have its content populated
+    # Wait for Sel. to find the price and the dynamic html to load
     WebDriverWait(driver, 20).until(
         EC.text_to_be_present_in_element(
             (By.CSS_SELECTOR, price_css_selector),
-            "$"  # This is the text we're expecting to be present
+            "$"  # dollar sign is the giveaway for applestore html
         )
     )
 
-    # Now retrieve the element's text
+    # retrieve and store the price
     price_element = driver.find_element(By.CSS_SELECTOR, price_css_selector)
     price = price_element.text if price_element else 'Price not found'
     print(f'Price: {price}')
@@ -44,7 +44,7 @@ try:
     # Generate a document ID based on the model and timestamp
     document_id = f"iPhone15_{int(time.time())}"
 
-    # Prepare data for upload
+    # firebase upload data
     data = {
         'model': 'iPhone 15',
         'price': price,
@@ -52,7 +52,7 @@ try:
         'timestamp': firestore.SERVER_TIMESTAMP
     }
 
-    # Add a new document to the smartphones collection with the specified document ID
+    # Add a new phone to smartphone collect with unique ID
     db.collection('smartphones').document(document_id).set(data)
     print(f"Data uploaded to Firestore with document ID: {document_id}. Price: {price}")
 
